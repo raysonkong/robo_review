39168f3fbb720631ed6f5a914090cabdd75dd44a
Rayson Kong <kong.rayson@gmail.com>
2020-06-08 09:33:11 +0800

    Updates

---
+++ precache-manifest.68c784008e6c010733608d7d0473e9cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4746f038a6dfe823570f9e406abf91d8",
+    "url": "/robo_review/index.html"
+  },
+  {
+    "revision": "97b553105a35369720e7",
+    "url": "/robo_review/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "721d4534fc22936d3b86",
+    "url": "/robo_review/static/css/main.0fbf46b0.chunk.css"
+  },
+  {
+    "revision": "97b553105a35369720e7",
+    "url": "/robo_review/static/js/2.4be2c2ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_review/static/js/2.4be2c2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721d4534fc22936d3b86",
+    "url": "/robo_review/static/js/main.9c704e58.chunk.js"
+  },
+  {
+    "revision": "6954b648ea09ddbcc3b5",
+    "url": "/robo_review/static/js/runtime-main.cd7bb220.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo_review/static/media/SEGA.12a3db8b.woff"
+  }
+]);
